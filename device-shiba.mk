ad5b70a22236324e2a9dfa5f01205fd40f59855c
Libra420T <ryantf420@gmail.com>
2026-04-26 16:46:40 -0400

    Initial Bringup of Alch3myOS

--- device-shiba.mk
+++ device-shiba.mk
@@ -5,15 +5,47 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Kernel
+# GMS/GAPPS
+WITH_GMS := true
+
+TARGET_SUPPORTED_REFRESH_RATES := 60,120
+
+SURFACE_FLINGER_BOOST := true
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,60)
+
+# Always use scudo for memory allocator
+PRODUCT_USE_SCUDO := true
+
+TARGET_OPTIMIZED_DEXOPT := true
+
+# Build Pixel Launcher
+TARGET_INCLUDE_PIXEL_LAUNCHER := true
+
+# Include Google Telephony
+WITH_GMS_COMMS_SUITE := true
+
+# Include Chargeing Bypass
+BYPASS_CHARGE_SUPPORTED := true
+
+# Supports HBM
+HBM_SUPPORTED := true
+HBM_NODE := /sys/class/backlight/panel0-backlight/hbm_mode
+
+# Pixel Kernel
 TARGET_LINUX_KERNEL_VERSION := 6.1
 TARGET_KERNEL_DEVICE := shusky
-TARGET_KERNEL_DIR := device/google/$(TARGET_KERNEL_DEVICE)-kernels/$(TARGET_LINUX_KERNEL_VERSION)
-TARGET_KERNEL_PLATFORM_SOURCE := google/gs-$(TARGET_LINUX_KERNEL_VERSION)
+TARGET_KERNEL_PATH := device/google/shusky-kernels
+TARGET_KERNEL_DIR := $(TARGET_KERNEL_PATH)/6.1
+TARGET_BOARD_KERNEL_HEADERS := $(TARGET_KERNEL_DIR)/kernel-headers
+TARGET_PREBUILT_KERNEL := $(TARGET_KERNEL_DIR)/Image.lz4
+LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image.lz4
 
-ifneq ($(TARGET_BOOTS_16K),true)
-PRODUCT_16K_DEVELOPER_OPTION := true
-endif
+LOCAL_PATH := device/google/shusky
+
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/shiba/overlay
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/shiba/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/shusky/overlay-lineage
 
 # Inherit from zuma
 include device/google/zuma/common.mk
@@ -26,7 +58,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     FrameworkResOverlayVendorShusky \
     PixelNfcOverlayShusky \
-    PixelWifiOverlay2023Shusky \
+    PixelWifiOverlay2023 \
     SafetyRegulatoryInfoOverlayProductShusky
 
 PRODUCT_PACKAGES += \
@@ -37,7 +69,8 @@ PRODUCT_PACKAGES += \
     PixelNfcOverlayShiba \
     SettingsGoogleShibaOverlay \
     SettingsShibaOverlay \
-    SystemUIGoogleOverlayVendorShiba
+    SystemUIGoogleOverlayVendorShiba \
+    Alch3mySettingsShiba
 
 PRODUCT_PACKAGES += \
     ApertureOverlayShiba
@@ -48,6 +81,33 @@ include hardware/google/pixel/powershare/device.mk
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/product.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/$(DEVICE_CODENAME)/vendor.prop
+
+PRODUCT_PRODUCT_PROPERTIES += \
+	ro.opa.eligible_device=true \
+	ro.com.google.clientidbase=android-google \
+	ro.com.google.ime.theme_id=5 \
+	ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms \
+	ro.support_one_handed_mode=true \
+	ro.quick_start.device_id=shiba \
+	ro.product.brand_for_attestation=google \
+	ro.product.device_for_attestation=shiba \
+	ro.product.manufacturer_for_attestation=Google \
+	ro.product.model_for_attestation=Pixel 8 \
+	ro.product.name_for_attestation=shiba
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	keyguard.no_require_sim=true \
+	debug.sf.enable_sdr_dimming=1 \
+	debug.sf.dim_in_gamma_in_enhanced_screenshots=1 \
+	ro.hardware.keystore_desede=true \
+	ro.hardware.keystore=trusty \
+	ro.hardware.gatekeeper=trusty \
+	persist.vendor.enable.thermal.genl=true \
+	ro.incremental.enable=true \
+	vendor.usb.product_string=Pixel 8
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+ro.hotword.detection_service_required=false
 
 # Recovery
 PRODUCT_COPY_FILES += \
